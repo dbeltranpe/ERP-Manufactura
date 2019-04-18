185e44c42eab0954ba92f74529ed400caee13d90
Daniel <Daniel@DESKTOP-Q1SEV8I>
2019-04-18 15:56:55 -0500

    Cambios en login

--- view/login.php
+++ view/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <!-- Required meta tags-->
     <meta charset="UTF-8">
@@ -49,35 +50,28 @@
                         <div class="login-form">
                             <form action="" method="post">
                                 <div class="form-group">
-                                    <label>Email Address</label>
-                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                                    <label>Usuario</label>
+                                    <input class="au-input au-input--full" type="email" name="email" placeholder="User">
                                 </div>
                                 <div class="form-group">
-                                    <label>Password</label>
+                                    <label>Contrase&ntilde;a</label>
                                     <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
                                 </div>
                                 <div class="login-checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember">Remember Me
-                                    </label>
-                                    <label>
-                                        <a href="#">Forgotten Password?</a>
+                                        <a href="#">Se me olvido mi contrase&ntilde;a</a>
                                     </label>
                                 </div>
-                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
-                                <div class="social-login-content">
-                                    <div class="social-button">
-                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button>
-                                        <button class="au-btn au-btn--block au-btn--blue2">sign in with twitter</button>
-                                    </div>
-                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">Ingresar</button>
+                                
                             </form>
+                            <!-- Documento esto porque todavía no se sabe si registrarse se utilizará
                             <div class="register-link">
                                 <p>
                                     Don't you have account?
                                     <a href="#">Sign Up Here</a>
                                 </p>
-                            </div>
+                            </div>-->
                         </div>
                     </div>
                 </div>
